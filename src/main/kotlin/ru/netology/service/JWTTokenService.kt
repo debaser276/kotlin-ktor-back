3b8df4104939c8014ca99a021c6611335325969f
debaser <debaser86@yandex.ru>
2020-02-24 11:30:52 +0300

    hw: 8 withExpiresDate

--- src/main/kotlin/ru/netology/service/JWTTokenService.kt
+++ src/main/kotlin/ru/netology/service/JWTTokenService.kt
@@ -3,6 +3,7 @@ package ru.netology.service
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import java.util.*
 
 class JWTTokenService {
     private val secret = "a5ce3f44-0e15-4805-8e74-3bec4039e244"
@@ -10,5 +11,8 @@ class JWTTokenService {
 
     val verifier: JWTVerifier = JWT.require(algo).build()
 
-    fun generate(id: Int): String = JWT.create().withClaim("id", id).sign(algo)
+    fun generate(id: Int): String = JWT.create()
+        .withClaim("id", id)
+        .withExpiresAt(Calendar.getInstance().apply { add(Calendar.MINUTE, 1) }.time)
+        .sign(algo)
 }
