85148da8ca583051acaabdbb9607302fa1d8b99e
debaser <debaser86@yandex.ru>
2020-03-17 18:15:27 +0300

    hw: 12 Change database settings

--- src/main/kotlin/ru/netology/repository/DatabaseFactory.kt
+++ src/main/kotlin/ru/netology/repository/DatabaseFactory.kt
@@ -22,17 +22,17 @@ object DatabaseFactory {
 
     private fun hikari(): HikariDataSource {
         val hikariConfig = HikariConfig().apply {
-            username = "debaser"
-            password = "gjlyfchtv"
-            jdbcUrl = "jdbc:postgresql://localhost/test"
-            driverClassName = "org.postgresql.Driver"
-
-//            val databaseUrl = System.getenv("DATABASE_URL")
-//            val dbUri = URI(databaseUrl)
-//            username = dbUri.userInfo.split(":")[0]
-//            password = dbUri.userInfo.split(":")[1]
-//            jdbcUrl = "jdbc:postgresql://${dbUri.host}${dbUri.path}"
+//            username = "debaser"
+//            password = "gjlyfchtv"
+//            jdbcUrl = "jdbc:postgresql://localhost/test"
 //            driverClassName = "org.postgresql.Driver"
+
+            val databaseUrl = System.getenv("DATABASE_URL")
+            val dbUri = URI(databaseUrl)
+            username = dbUri.userInfo.split(":")[0]
+            password = dbUri.userInfo.split(":")[1]
+            jdbcUrl = "jdbc:postgresql://${dbUri.host}${dbUri.path}"
+            driverClassName = "org.postgresql.Driver"
         }
         return HikariDataSource(hikariConfig)
     }
